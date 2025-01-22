b8c0ae457b68e3d781042342d3405cb5f4a6571d
Unmentionable Arletha <unmentia@ti-l3.skd.21-school.uz>
2025-01-22 22:08:26 +0500

    update

--- array/task6.go
+++ array/task6.go
@@ -4,11 +4,17 @@ package main
 import "fmt"
 
 func main(){
-	nums := [] int {6, 25, 91, 23, 72, 9, 18, 6, 0}
+	nums := [] int {6, 25, 91, 23, 72, 9, 18, 6}
+	arr := make([]int, len(nums)+1)
 	n := 0
 	fmt.Scan(&n)
 
-	nums[len(nums)-1] = n
-
-	fmt.Println(nums)
+	for i:=0; i<len(arr); i++ {
+		if i<len(arr)-1 {
+			arr[i] = nums[i]
+		} else {
+			arr[i] = n
+		}
+	}
+	fmt.Println(arr)
 }
--- array/task7.go
+++ array/task7.go
@@ -5,9 +5,13 @@ import "fmt"
 
 func main(){
 	nums := [] int {0, 6, 25, 91, 23, 72, 9, 18, 6}
+	arr := make([]int, len(nums)+1)
 	n := 0
 	fmt.Scan(&n)
 
-	nums[0] = n
-	fmt.Println(nums)
+	arr[0] = n
+	for i:=0; i<len(nums); i++ {
+		arr[i+1] = nums[i]
+	}
+	fmt.Println(arr)
 }
